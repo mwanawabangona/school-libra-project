61924f557b88473d95c89132dda8f628ff8e9531
mwanawabangona <ellisallday@gmail.com>
2022-03-03 03:15:38 -0800

    Person data

--- person.rb
+++ person.rb
@@ -1,19 +1,34 @@
+require_relative './corrector'
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.ran(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
-  private :of_age?
+
+  def validate_name
+    @name = @corrector.correct_name(name)
+  end
+
+  private
+
   def of_age?
     @age >= 18
   end
+
+  public
 
   def can_use_services?
     of_age? || parent_permission == true
   end
 end
+
+person = Person.new(100, 'Ellisngona')
+person.validate_name
+
+puts person.name
